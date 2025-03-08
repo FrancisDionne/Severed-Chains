d642ede103b38a654f3ab0b8650cf1daaa804c11
Francis Dionne <96087943+FrancisDionne@users.noreply.github.com>
2025-03-08 14:01:35 -0800

    Fixed couple things on AdditionSettingsScreen

--- src/main/java/legend/game/inventory/screens/AdditionSettingsScreen.java
+++ src/main/java/legend/game/inventory/screens/AdditionSettingsScreen.java
@@ -1,18 +1,19 @@
 package legend.game.inventory.screens;
 
 import legend.core.GameEngine;
+import legend.game.combat.ui.FooterActions;
+import legend.game.combat.ui.FooterActionsHud;
 import legend.game.i18n.I18n;
-import legend.core.platform.input.InputAction;
 import legend.game.inventory.screens.controls.Background;
 import legend.game.inventory.screens.controls.Label;
-import legend.game.modding.coremod.CoreMod;
 import legend.game.saves.ConfigCategory;
 import legend.game.saves.ConfigCollection;
 import legend.game.saves.ConfigEntry;
 import legend.game.saves.ConfigStorageLocation;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.legendofdragoon.modloader.registries.RegistryId;
 
-import java.util.Comparator;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
@@ -20,10 +21,15 @@ import java.util.Set;
 import static legend.game.Scus94491BpeSegment.startFadeEffect;
 import static legend.game.Scus94491BpeSegment_8002.deallocateRenderables;
 import static legend.game.Scus94491BpeSegment_8002.playMenuSound;
+import static legend.game.Scus94491BpeSegment_8002.textWidth;
 import static legend.game.modding.coremod.CoreMod.INPUT_ACTION_MENU_BACK;
+import static legend.game.modding.coremod.CoreMod.INPUT_ACTION_MENU_HELP;
 
 public class AdditionSettingsScreen extends VerticalLayoutScreen {
   private final Runnable unload;
+  private final Map<Control, ConfigEntry<?>> helpEntries = new HashMap<>();
+  private final Map<Control, Label> helpLabels = new HashMap<>();
+  private static final Logger LOGGER = LogManager.getFormatterLogger(OptionsScreen.class);
 
   public AdditionSettingsScreen(final ConfigCollection config, final Set<ConfigStorageLocation> validLocations, final ConfigCategory category, final Runnable unload) {
     deallocateRenderables(0xff);
@@ -33,38 +39,84 @@ public class AdditionSettingsScreen extends VerticalLayoutScreen {
 
     this.addControl(new Background());
 
-    final Map<RegistryId, String> translations = new HashMap<>();
+    final Map<ConfigEntry<?>, String> translations = new HashMap<>();
 
     for(final RegistryId configId : GameEngine.REGISTRIES.config) {
       final ConfigEntry<?> entry = GameEngine.REGISTRIES.config.getEntry(configId).get();
 
       if(entry.category == category) {
-        translations.put(configId, I18n.translate(configId.modId() + ".config." + configId.entryId() + ".label"));
+        translations.put(entry, I18n.translate(entry.getLabelTranslationKey()));
       }
     }
 
     translations.entrySet().stream()
-      .sorted(Comparator.comparingInt(o -> CoreMod.configOrder.indexOf(o.getKey().toString())))
+      .sorted((o1, o2) -> String.CASE_INSENSITIVE_ORDER.compare(o1.getValue(), o2.getValue()))
       .forEach(entry -> {
-        final RegistryId configId = entry.getKey();
-        final String label = entry.getValue();
-
         //noinspection rawtypes
-        final ConfigEntry configEntry = GameEngine.REGISTRIES.config.getEntry(configId).get();
+        final ConfigEntry configEntry = entry.getKey();
+        final String text = entry.getValue();
 
-        if(validLocations.contains(configEntry.storageLocation)) {
-          //noinspection unchecked
-          if (configEntry.hasEditControl()) {
-            this.addRow(label, configEntry.makeEditControl(config.getConfig(configEntry), config)).setZ(35);
-          } else {
-            this.addRow(label, new Label("")).setZ(35);
+        if(validLocations.contains(configEntry.storageLocation) && configEntry.hasEditControl()) {
+          Control editControl;
+          boolean error = false;
+
+          try {
+            //noinspection unchecked
+            editControl = configEntry.makeEditControl(config.getConfig(configEntry), config);
+          } catch(final Throwable ex) {
+            editControl = this.createErrorLabel("Error creating control", ex, false);
+            error = true;
+          }
+
+          editControl.setZ(35);
+
+          final Label label = this.addRow(text, editControl);
+
+          if(error) {
+            label.getFontOptions().colour(0.30f, 0.0f, 0.0f).shadowColour(TextColour.LIGHT_BROWN);
+          }
+
+          if(configEntry.hasHelp()) {
+            final Label help = label.addControl(new Label("?"));
+            help.setScale(0.4f);
+            help.setPos((int)(textWidth(text) * label.getScale()) + 2, 1);
+            help.onHoverIn(() -> this.getStack().pushScreen(new TooltipScreen(I18n.translate(configEntry.getHelpTranslationKey()), this.mouseX, this.mouseY)));
+            this.helpLabels.put(label, help);
+            this.helpEntries.put(label, configEntry);
           }
         }
       });
+
+    FooterActionsHud.setMenuActions(FooterActions.HELP, null, null);
+    this.addHotkey(I18n.translate("lod_core.ui.options.help"), INPUT_ACTION_MENU_HELP, this::help);
+    this.addHotkey(I18n.translate("lod_core.ui.options.back"), INPUT_ACTION_MENU_BACK, this::back);
   }
 
   private void back() {
     playMenuSound(3);
     this.unload.run();
+  }
+
+  private void help() {
+    final ConfigEntry<?> configEntry = this.helpEntries.get(this.getHighlightedRow());
+    if(configEntry != null) {
+      playMenuSound(2);
+      final Label helpLabel = this.helpLabels.get(this.getHighlightedRow());
+      this.getStack().pushScreen(new TooltipScreen(I18n.translate(configEntry.getHelpTranslationKey()), helpLabel.calculateTotalX() + helpLabel.getWidth() / 2, helpLabel.calculateTotalY() + helpLabel.getHeight() / 2));
+    }
+  }
+
+  private Label createErrorLabel(final String log, final Throwable ex, final boolean setSize) {
+    LOGGER.warn(log, ex);
+    final Label l = new Label(I18n.translate("lod_core.ui.options.error"));
+    l.getFontOptions().colour(0.30f, 0.0f, 0.0f).shadowColour(TextColour.LIGHT_BROWN);
+
+    if(setSize) {
+      l.setSize(140, 11);
+      l.setPos(this.getWidth() - 64 - l.getWidth(), 0);
+      l.setScale(0.66f);
+    }
+
+    return l;
   }
 }
