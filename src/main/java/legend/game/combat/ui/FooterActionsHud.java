c975b209497b588e200a60c6e581861ecbfadceb
Francis Dionne <96087943+FrancisDionne@users.noreply.github.com>
2025-12-01 22:20:36 -0800

    Tracker Visual

--- src/main/java/legend/game/combat/ui/FooterActionsHud.java
+++ src/main/java/legend/game/combat/ui/FooterActionsHud.java
@@ -101,6 +101,7 @@ public final class FooterActionsHud {
       case FooterActions.DEFAULT -> "Default";
       case FooterActions.JUMP -> "Jump";
       case FooterActions.TRACK -> "Track";
+      case FooterActions.UNTRACK -> "Untrack";
     };
   }
 
@@ -209,7 +210,7 @@ public final class FooterActionsHud {
       case FooterActions.BACK -> INPUT_ACTION_MENU_BACK.get();
       case FooterActions.DELETE, FooterActions.FILTER, FooterActions.DISCARD, FooterActions.ARCHIVE, FooterActions.TURN_ORDER, FooterActions.PRESETS, FooterActions.DEFAULT, FooterActions.JUMP -> INPUT_ACTION_MENU_DELETE.get();
       case FooterActions.SORT, FooterActions.ADDITIONS, FooterActions.HELP, FooterActions.MODS, FooterActions.LIST, FooterActions.DISPLAY_MODE -> INPUT_ACTION_MENU_SORT.get();
-      case FooterActions.SELECT, FooterActions.TRACK -> INPUT_ACTION_MENU_CONFIRM.get();
+      case FooterActions.SELECT, FooterActions.TRACK, FooterActions.UNTRACK -> INPUT_ACTION_MENU_CONFIRM.get();
     };
     return new FooterAction(action, input);
   }
